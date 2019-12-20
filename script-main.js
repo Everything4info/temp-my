e2cdd0291fdcd615d0f04913c9b72a8c16da40a7
Everything4info <54599875+Everything4info@users.noreply.github.com>
2019-12-20 20:15:19 +0200

    Create script-main.js

---
+++ script-main.js
@@ -0,0 +1,12 @@
+$(function() {
+        "use strict";
+        $("#postAD-top")["append"]($("#HTML201"));
+        $("#postAD-bottom")["append"]($("#HTML203"));
+        var _0x8468x0 = Math["floor"]($(".post-body *")["length"] / 2);
+        $(".post-body *:eq(" + _0x8468x0 + ")")["after"]($("#HTML202"));
+       
+});
+
+!function(h){h("a").click(function(t){var e=h(this);0==e.find(".ink").length&&e.append("<span class='ink'></span>");var i=e.find(".ink");if(i.removeClass("animate"),!i.height()&&!i.width()){var a=Math.max(e.outerWidth(),e.outerHeight());i.css({height:a,width:a})}var n=t.pageX-e.offset().left-i.width()/2,s=t.pageY-e.offset().top-i.height()/2;i.css({top:s+"px",left:n+"px"}).addClass("animate")})}(jQuery); 
+!function(h){h(".btn").click(function(t){var e=h(this);0==e.find(".ink").length&&e.append("<span class='ink'></span>");var i=e.find(".ink");if(i.removeClass("animate"),!i.height()&&!i.width()){var a=Math.max(e.outerWidth(),e.outerHeight());i.css({height:a,width:a})}var n=t.pageX-e.offset().left-i.width()/2,s=t.pageY-e.offset().top-i.height()/2;i.css({top:s+"px",left:n+"px"}).addClass("animate")})}(jQuery); 
+	 
